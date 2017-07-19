a76a6fe0688b34694f6a562e560cedddb899c367
Matt McSpirit <mattmc@outlook.com>
2017-07-19 14:29:39 -0700

    Updated to remove Dev flags

--- install-ucp-mgr.sh
+++ install-ucp-mgr.sh
@@ -17,9 +17,9 @@ sudo service docker start
 docker login -p $HUB_PASSWORD -u $HUB_USERNAME
 
 #install UCP
-docker run dockerorcadev/ucp:$UCP_VERSION images --list --image-version dev: | xargs -L 1 docker pull
+docker run docker/ucp:$UCP_VERSION images --list
 docker run --rm --name ucp \
   -e REGISTRY_USERNAME=$HUB_USERNAME -e REGISTRY_PASSWORD=$HUB_PASSWORD \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  dockerorcadev/ucp:$UCP_VERSION \
-  install --san $UCP_PUBLIC_FQDN --admin-password $UCP_ADMIN_PASSWORD --debug --image-version dev:
+  docker/ucp:$UCP_VERSION \
+  install --san $UCP_PUBLIC_FQDN --admin-password $UCP_ADMIN_PASSWORD --debug
